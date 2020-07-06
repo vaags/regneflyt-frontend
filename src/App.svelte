02d3232ad7258c39e01a4fd18b2a6af14a3969a9
Thomas <tav@novacare.no>
2020-07-06 19:02:02 +0200

    Added button component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,20 @@
 <script>
+	import Button from './components/Button.svelte';
+
 	export let name;
+
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<Button on:click={handleClick} label="Click me {count}" />
 </main>
 
 <style>
---
+++ src/components/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let label = "[Label]";
+</script>
+
+<button
+    on:click
+    class="bg-blue-500
+           hover:bg-blue-700
+           text-white
+           font-bold
+           py-2
+           px-4
+           rounded
+           ">
+    {label}
+</button>
