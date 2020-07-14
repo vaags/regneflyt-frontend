43a4cf8eb29f6521d45dca74f31036079cdec3e7
Thomas <tav@novacare.no>
2020-07-14 08:35:15 +0200

    Fixed some range slider styling issues

--- src/components/widgets/Range.svelte
+++ src/components/widgets/Range.svelte
@@ -6,10 +6,10 @@
 </script>
 
 <div class="mb-4">
-    <span class="text-blue-800 mr-4 float-left mt-1">{value}</span>
+    <span class="text-blue-800 mr-4 float-left mt-1 w-5">{value}</span>
     <input
         on:change
-        class="w-3/4 md:w-1/2 mt-2"
+        class="w-3/4 mt-2"
         type="range"
         min={min}
         max={max}
