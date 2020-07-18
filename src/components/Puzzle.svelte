e9e6dbaa740786f32f98a3d783dfb01d7cef52eb
Thomas <tav@novacare.no>
2020-07-18 15:25:46 +0200

    Wip: Time limit per puzzle

--- src/components/Puzzle.svelte
+++ src/components/Puzzle.svelte
@@ -1,13 +1,18 @@
 <script>
-    import { createEventDispatcher, onMount } from 'svelte';
+    import { createEventDispatcher, onMount, onDestroy } from 'svelte';
     import Button from './widgets/Button.svelte';
+    import Alert from './widgets/Alert.svelte';
     import Operator from './widgets/Operator.svelte';
 
     const dispatch = createEventDispatcher();
+    let interval = undefined;
 
     export let quiz
 
     let puzzleNumber = 0;
+    let input;
+    let validationError = false;
+    let startTime;
 
     let puzzle = {
         partOne: {
@@ -19,14 +24,12 @@
             value: undefined
         },
         answer: undefined,
+        timeout: undefined,
         duration: undefined,
         isCorrect: undefined,
         operator: quiz.activeOperator,
     }
 
-    let input;
-    let validationError = false;
-    let startTime;
 
     $: displayError = !puzzle.answer && validationError;
 
@@ -39,9 +42,15 @@
         puzzle.answer = undefined
         puzzle.isCorrect = undefined
         puzzle.duration = undefined
+        puzzle.timeout = undefined
         puzzleNumber++
+        console.log('focusing');
         input.focus()
         startTime = Date.now();
+        if (quiz.puzzleTimeLimit) {
+            if (interval) clearInterval(interval);
+            interval = setTimeout(timeOutPuzzle, quiz.puzzleTimeLimit * 1000)
+        }
     }
 
     function shouldAvoidNegativeAnswer() {
@@ -53,8 +62,8 @@
         [puzzle.partOne, puzzle.partTwo] = [puzzle.partTwo, puzzle.partOne];
     }
 
+    // TODO: Refactor for readability
     function getPuzzlePart(quizPuzzlePart, previousPuzzlePart) {
-
         if (quizPuzzlePart.minValue === quizPuzzlePart.maxValue)
             return {
                 index: 0,
@@ -85,13 +94,23 @@
         }
     }
 
-    function completePuzzle() {
+    function completePuzzleIfValid() {
         if (!puzzleIsValid()) return;
 
+        completePuzzle();
+    }
+
+    function timeOutPuzzle() {
+        puzzle.timeout = true;
+        puzzle.answer = undefined;
+    }
+
+    function completePuzzle() {
         puzzle.isCorrect = evaluateAnswer();
         puzzle.duration = (Date.now() - startTime) / 1000;
 
         dispatch('addPuzzle', { puzzle: {...puzzle} });
+
         generatePuzzle()
     }
 
@@ -111,7 +130,6 @@
     function puzzleIsValid() {
         if (puzzle.answer == undefined) {
             validationError = true;
-            input.focus()
             return;
         }
 
@@ -124,12 +142,21 @@
         generatePuzzle()
     })
 
+    onDestroy(() => {
+        clearInterval(interval);
+    });
+
 </script>
 <form>
     <div class="card pb-6">
         <h2>Oppgave {puzzleNumber}</h2>
+            {#if puzzle.timeout}
+                <Alert color="red" message="Tiden er ute." />
+            {/if}
             <div class="text-center my-12 text-4xl">{puzzle.partOne.value} <Operator operator={quiz.activeOperator} /> {puzzle.partTwo.value} = 
             <input
+                id="answerInput"
+                disabled={puzzle.timeout}
                 bind:this={input}
                 bind:value={puzzle.answer}
                 class="border {displayError ? 'validation-error-border' : ''} rounded w-24 py-2 px-3 leading-tight focus:outline-none"
@@ -140,7 +167,7 @@
             </div>
     </div>
     <div class="float-left">
-        <Button on:click="{completePuzzle}" label="Neste" color="{displayError ? "red" : "green"}" />
+        <Button on:click="{puzzle.timeout ? completePuzzle : completePuzzleIfValid}" label="Neste" color="{displayError ? "red" : "green"}" />
     </div>
 </form>
 
