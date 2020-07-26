cd91441f06c73afb150af59bae6dd28fffae241d
Thomas Vaags <thomas.vaags@gmail.com>
2020-07-26 19:31:18 +0200

    Css minification

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,16 @@
+const mode = process.env.NODE_ENV
+const dev = mode === 'development'
+
 const tailwindcss = require("tailwindcss");
 
 module.exports = {
   plugins: [
     tailwindcss("./tailwind.config.js"),
-    require('autoprefixer')
+    require('autoprefixer'),
+    // Minify if prod
+    !dev &&
+    require('cssnano')({
+      preset: ['default', { discardComments: { removeAll: true } }],
+    }),
   ],
 };
