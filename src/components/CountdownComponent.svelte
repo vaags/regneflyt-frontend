1b5b5b7c2affdf5d50f89ed72d2998b46a0b7c3c
Thomas Vaags <thomas.vaags@gmail.com>
2020-07-27 16:02:47 +0200

    Added fade-out animation

--- src/components/CountdownComponent.svelte
+++ src/components/CountdownComponent.svelte
@@ -1,18 +1,38 @@
 <script lang="ts">
-    import { onDestroy } from 'svelte'
+    import { onDestroy, onMount } from 'svelte'
     export let time: number
+    let whiteText = false
+    let timeout: number
 
     const interval = setInterval(() => {
         if (time > 1) time-- // Should not show zero
+        fadeOut()
     }, 1000)
 
     onDestroy(() => {
         clearInterval(interval)
+        clearTimeout(timeout)
+    })
+
+    function fadeOut() {
+        whiteText = false
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+            whiteText = true
+        }, 500)
+    }
+
+    onMount(() => {
+        fadeOut()
     })
 </script>
 
 <div class="card">
     <h2>Gjør deg klar&hellip;</h2>
 
-    <p class="text-center my-12 text-4xl md:text-5xl">{time}</p>
+    <p
+        class="text-center my-12 text-4xl md:text-5xl {whiteText ? 'text-white' : ''}
+        transition duration-1000 ease-out">
+        {time}
+    </p>
 </div>
