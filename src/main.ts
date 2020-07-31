7dc719fd644d052de581a210bf30645ee9132838
Thomas Vaags <thomas.vaags@gmail.com>
2020-07-31 08:18:05 +0200

    Basic PWA setup Stolen from https://github.com/OliwerDrywa/svelte-pwa

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body
-});
+	target: document.body,
+	props: {}
+})
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/service-worker.js')
+}
 
 export default app;
