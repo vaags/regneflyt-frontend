cfedcbffdd45b42ef82c52b601d5434aa68c92da
Thomas Vaags <thomas.vaags@gmail.com>
2020-08-19 18:02:24 +0200

    Removed button border

--- src/components/widgets/ButtonComponent.svelte
+++ src/components/widgets/ButtonComponent.svelte
@@ -8,7 +8,7 @@
 <button
     {disabled}
     on:click|preventDefault
-    class="text-white font-light {small ? 'text-xl px-3' : 'text-3xl px-4'} pb-1
+    class="text-white font-light {small ? 'text-xl px-3' : 'text-3xl px-4'}
     border-b-2 rounded btn-{color} transition-colors duration-200 ease-out
     focus:outline-none">
     {label}
