f3be8efc1f53942045cd9e49fbfe71f1f83e164f
Thomas Vaags <thomas.vaags@gmail.com>
2020-08-28 13:42:46 +0200

    Cosmetic fix

--- src/components/widgets/OperatorComponent.svelte
+++ src/components/widgets/OperatorComponent.svelte
@@ -5,16 +5,14 @@
     export let returnName: boolean = false
 </script>
 
-<span>
-    {#if operator === Operator.Addition}
-        {@html returnName ? 'Addisjon' : '&#43;'}
-    {:else if operator === Operator.Subtraction}
-        {@html returnName ? 'Subtraksjon' : '&#8722;'}
-    {:else if operator === Operator.Multiplication}
-        {@html returnName ? 'Multiplikasjon' : '&#215;'}
-    {:else if operator === Operator.Division}
-        {@html returnName ? 'Divisjon' : '&#247;'}
-    {:else if operator === Operator.All}
-        {@html returnName ? 'Alle' : '&#8704'}
-    {/if}
-</span>
+{#if operator === Operator.Addition}
+    {@html returnName ? 'Addisjon' : '&#43;'}
+{:else if operator === Operator.Subtraction}
+    {@html returnName ? 'Subtraksjon' : '&#8722;'}
+{:else if operator === Operator.Multiplication}
+    {@html returnName ? 'Multiplikasjon' : '&#215;'}
+{:else if operator === Operator.Division}
+    {@html returnName ? 'Divisjon' : '&#247;'}
+{:else if operator === Operator.All}
+    {@html returnName ? 'Alle' : '&#8704'}
+{/if}
