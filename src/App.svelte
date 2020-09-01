8b6f24dd2cc08dec526258b5e18fd7e3d301f936
Thomas Vaags <thomas.vaags@gmail.com>
2020-09-01 08:59:51 +0200

    Bump version

--- src/App.svelte
+++ src/App.svelte
@@ -80,26 +80,31 @@
 
 <main class="container max-w-xl mx-auto px-2 md:px-3 pt-1 pb-2 md:pb-5">
     <h1 class="text-2xl md:text-3xl font-light text-orange-600 mb-1 text-right">
-        Regneflyt
-        <small class="text-base text-gray-500">1.5</small>
+        Regneflyt <small class="text-base text-gray-500">1.6</small>
     </h1>
     {#if quiz.state === QuizState.AboutToStart}
         <GetReadyComponent
-            {appSettings}
+            appSettings="{appSettings}"
             on:startQuiz="{startQuiz}"
             on:completeQuiz="{completeQuiz}"
             on:abortQuiz="{abortQuiz}" />
     {:else if quiz.state === QuizState.Started}
         <QuizComponent
-            {quiz}
+            quiz="{quiz}"
             on:abortQuiz="{abortQuiz}"
             on:completeQuiz="{completeQuiz}"
-            {appSettings} />
+            appSettings="{appSettings}" />
     {:else if quiz.state === QuizState.Completed}
         <GameOverComponent on:evaluateQuiz="{evaluateQuiz}" />
     {:else if quiz.state === QuizState.Evaluated}
-        <ResultsComponent {quiz} {puzzleSet} on:resetQuiz="{resetQuiz}" />
+        <ResultsComponent
+            quiz="{quiz}"
+            puzzleSet="{puzzleSet}"
+            on:resetQuiz="{resetQuiz}" />
     {:else}
-        <MenuComponent {quiz} on:getReady="{getReady}" {appSettings} />
+        <MenuComponent
+            quiz="{quiz}"
+            on:getReady="{getReady}"
+            appSettings="{appSettings}" />
     {/if}
 </main>
