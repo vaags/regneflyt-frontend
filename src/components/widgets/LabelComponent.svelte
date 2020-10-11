75fac44cdae46efc136daa74d6509d217dd118c0
Thomas Vaags <thomas.vaags@gmail.com>
2020-10-11 15:37:04 +0200

    Larger label text

--- src/components/widgets/LabelComponent.svelte
+++ src/components/widgets/LabelComponent.svelte
@@ -1,6 +1,6 @@
 <span
     on:click
-    class="text-sm float-right font-medium bg-blue-100 py-1 px-2 ml-2 rounded
+    class="float-right font-medium bg-blue-100 py-1 px-2 ml-2 rounded
     text-blue-700 align-middle">
     <slot />
 </span>
