57489a136cc5154406e41644d40fd894bc9ac717
Thomas Vaags <thomas.vaags@gmail.com>
2020-11-21 09:48:58 +0100

    version bump

--- src/App.svelte
+++ src/App.svelte
@@ -64,7 +64,7 @@
 <main class="container max-w-xl mx-auto px-2 md:px-3 pt-2 pb-2 md:pb-5">
     <h1 class="text-2xl md:text-3xl font-light text-orange-500 mb-2 text-right">
         Regneflyt
-        <small class="text-sm md:text-base text-gray-400">1.39</small>
+        <small class="text-sm md:text-base text-gray-400">1.40</small>
     </h1>
     {#if quiz.state === QuizState.AboutToStart}
         <GetReadyComponent
