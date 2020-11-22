14f201e165c2ef4c102cbd8d293221871e793f09
Thomas Vaags <thomas.vaags@gmail.com>
2020-11-22 09:06:40 +0100

    Cosmetic tweaks

--- src/App.svelte
+++ src/App.svelte
@@ -61,11 +61,11 @@
     })
 </script>
 
-<main class="container max-w-xl mx-auto px-2 md:px-3 py-1 md:py-2 md:pb-5">
+<main class="container max-w-xl mx-auto px-2 md:px-3 pt-1 pb-2 md:pt-2 md:pb-5">
     <h1
         class="text-2xl md:text-3xl font-light text-orange-500 mb-1 md:mb-2 text-right">
         Regneflyt
-        <small class="text-sm md:text-base text-gray-400">1.43</small>
+        <small class="text-sm md:text-base text-gray-400">1.44</small>
     </h1>
     {#if quiz.state === QuizState.AboutToStart}
         <GetReadyComponent
