037fd40d2632741139367558a566a193560bbbc2
Thomas Vaags <thomas.vaags@gmail.com>
2020-12-01 08:02:25 +0100

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
 import pkg from "./package.json";
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -45,15 +46,15 @@ export default {
 			VERSION: pkg.version,
 		}),
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('bundle.css');
-			},
 			preprocess: sveltePreprocess({ postcss: true, sourceMap: !production }),
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			}
 		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
