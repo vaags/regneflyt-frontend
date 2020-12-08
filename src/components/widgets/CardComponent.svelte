e089a64e02ba0aac3b9ede0e838a91bb6a108c5e
Thomas Vaags <thomas.vaags@gmail.com>
2020-12-08 09:14:51 +0100

    Flexbox card fixes

--- src/components/widgets/CardComponent.svelte
+++ src/components/widgets/CardComponent.svelte
@@ -8,18 +8,20 @@
 <div
     class="mb-1 md:mb-2 rounded-tl-md rounded-br-lg px-4 md:px-5 pt-3 md:pt-4 pb-3 md:pb-5 border-b-2 border-l-4 
     {isInfo ? 'bg-blue-50 border-blue-500' : 'bg-gray-100 border-gray-500'}">
-    {#if heading}
-        <h2
-            class="text-3xl tracking-tight md:text-4xl font-light -mt-1 mb-5 md:mb-6 text-gray-700">
-            {heading}
-        </h2>
-    {/if}
-    <div class="flex-grow text-right">
-        {#if label !== undefined}
-            <LabelComponent>{label}</LabelComponent>
-        {:else}
-            <slot name="label" />
+    <div class="flex justify-between">
+        {#if heading}
+            <h2
+                class="text-3xl tracking-tight md:text-4xl font-light -mt-1 mb-5 md:mb-6 text-gray-700">
+                {heading}
+            </h2>
         {/if}
+        <div>
+            {#if label !== undefined}
+                <LabelComponent>{label}</LabelComponent>
+            {:else}
+                <slot name="label" />
+            {/if}
+        </div>
     </div>
     <slot />
 </div>
