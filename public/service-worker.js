ea010b6fb2b297b64bfd232e3d0e91aa77317356
Thomas Vaags <thomas.vaags@gmail.com>
2021-01-16 08:39:34 +0100

    Change cache strategy for assets folder Update packages

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,16 @@
 
-const cacheName = 'network-first'
-
 // import workbox 	
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js')
 const { routing, strategies } = workbox
 
+routing.registerRoute(
+    ({ url }) => url.origin === self.location.origin &&
+        url.pathname.startsWith('/assets/'),
+    new strategies.CacheFirst('cache-first')
+)
+
 // implements NetworkFirst to all routes	
 routing.registerRoute(
     () => true,
-    new strategies.NetworkFirst({ cacheName }),
+    new strategies.NetworkFirst('network-first'),
 )
-
-// removes all caches not named <cacheName>	
-const invalidateOldCache = async () => {
-    const keys = await caches.keys()
-    const isOldCache = (key) => key !== cacheName
-    const oldKeys = keys.filter(isOldCache)
-
-    return Promise.all(oldKeys.map((key) => caches.delete(key)))
-}
-
-// runs invalidateOldCache on activation	
-self.addEventListener('activate', (e) => e.waitUntil(invalidateOldCache())) 
