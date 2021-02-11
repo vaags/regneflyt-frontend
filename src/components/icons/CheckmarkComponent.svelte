40722100e3243536d9a11289ce114985a6f1c49d
Thomas Vaags <thomas.vaags@gmail.com>
2021-02-11 06:35:32 +0100

    Fix checkmark icon positioning

--- src/components/icons/CheckmarkComponent.svelte
+++ src/components/icons/CheckmarkComponent.svelte
@@ -3,5 +3,6 @@
 </script>
 
 <div
-    class="w-4.5 h-2.5 border-green-600 border-b-4 border-l-4 transform -rotate-45 mb-2 ml-0.5"
-    aria-label="{label}"></div>
+    class="w-4.5 h-2.5 border-green-600 border-b-4 border-l-4 transform -rotate-45 mb-2 ml-0.5 mt-1"
+    aria-label="{label}"
+></div>
