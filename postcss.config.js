76c3ae7ed8bafd402e6c9b18eb619b249c8aa186
vaags <thomas.vaags@gmail.com>
2021-03-29 13:07:48 +0200

    Added tailwind jit (#50)

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,13 @@ const mode = process.env.NODE_ENV
 const dev = mode === 'development'
 
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // Minify if prod
-    !dev &&
-    require('cssnano')({
-      preset: ['default', { discardComments: { removeAll: true } }],
-    }),
-  ],
-};
+    plugins: [
+        require('@tailwindcss/jit'),
+        require('autoprefixer'),
+        // Minify if prod
+        !dev &&
+            require('cssnano')({
+                preset: ['default', { discardComments: { removeAll: true } }],
+            }),
+    ],
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,72 +3,72 @@ const plugin = require('tailwindcss/plugin')
 const pseudoClasses = ['before', 'after']
 
 module.exports = {
-  purge: ["./src/**/*.svelte", "./public/index.html"],
-  darkMode: false,
-  theme: {
-    colors: {
-      black: '#000',
-      white: '#fff',
-      gray: colors.warmGray,
-      orange: colors.orange,
-      red: colors.red,
-      green: colors.emerald,
-      blue: colors.lightBlue,
-      yellow: colors.amber
-    },
-    extend: {
-      rotate: {
-        '25.5': '25.5deg',
-        '35': '35deg',
-        '-35': '-35deg',
-      },
-      spacing: {
-        '4.5': '1.125rem'
-      }
-    },
-  },
-  variants: {
-    extend: {
-      backgroundColor: pseudoClasses,
-      width: pseudoClasses,
-      height: pseudoClasses,
-      position: pseudoClasses,
-      transform: pseudoClasses,
-      inset: pseudoClasses,
-      rotate: pseudoClasses,
-      borderRadius: pseudoClasses,
-      borderWidth: pseudoClasses,
-      borderColor: pseudoClasses
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    plugin(({ addVariant, e }) => {
-      addVariant('before', ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          return `.${e(`before${separator}${className}`)}::before`;
-        });
-      });
-      addVariant('after', ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          return `.${e(`after${separator}${className}`)}::after`;
-        });
-      });
-    }),
-    plugin(({ addUtilities }) => {
-      const contentUtilities = {
-        '.content': {
-          content: 'attr(data-content)',
+    purge: ['./src/**/*.svelte', './public/index.html'],
+    darkMode: false,
+    theme: {
+        colors: {
+            black: '#000',
+            white: '#fff',
+            gray: colors.warmGray,
+            orange: colors.orange,
+            red: colors.red,
+            green: colors.emerald,
+            blue: colors.lightBlue,
+            yellow: colors.amber,
         },
-        '.content-before': {
-          content: 'attr(data-before)',
+        extend: {
+            rotate: {
+                25.5: '25.5deg',
+                35: '35deg',
+                '-35': '-35deg',
+            },
+            spacing: {
+                4.5: '1.125rem',
+            },
         },
-        '.content-after': {
-          content: 'attr(data-after)',
+    },
+    variants: {
+        extend: {
+            backgroundColor: pseudoClasses,
+            width: pseudoClasses,
+            height: pseudoClasses,
+            position: pseudoClasses,
+            transform: pseudoClasses,
+            inset: pseudoClasses,
+            rotate: pseudoClasses,
+            borderRadius: pseudoClasses,
+            borderWidth: pseudoClasses,
+            borderColor: pseudoClasses,
         },
-      };
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        plugin(({ addVariant, e }) => {
+            addVariant('before', ({ modifySelectors, separator }) => {
+                modifySelectors(({ className }) => {
+                    return `.${e(`before${separator}${className}`)}::before`
+                })
+            })
+            addVariant('after', ({ modifySelectors, separator }) => {
+                modifySelectors(({ className }) => {
+                    return `.${e(`after${separator}${className}`)}::after`
+                })
+            })
+        }),
+        plugin(({ addUtilities }) => {
+            const contentUtilities = {
+                '.content': {
+                    content: 'attr(data-content)',
+                },
+                '.content-before': {
+                    content: 'attr(data-before)',
+                },
+                '.content-after': {
+                    content: 'attr(data-after)',
+                },
+            }
 
-      addUtilities(contentUtilities, pseudoClasses);
-    }),
-  ]
+            addUtilities(contentUtilities, pseudoClasses)
+        }),
+    ],
 }
