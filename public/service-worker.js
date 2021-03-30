4d57b50c3d6d5364116a5db1f6b366e60c2ce234
Thomas Vaags <thomas.vaags@gmail.com>
2021-03-30 09:12:48 +0200

    Fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,15 @@
-
-// import workbox 	
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js')
+// import workbox
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js'
+)
 const { routing, strategies } = workbox
 
 routing.registerRoute(
-    ({ url }) => url.origin === self.location.origin &&
-        url.pathname.startsWith('/assets/'),
+    ({ url }) =>
+        url.origin === self.location.origin &&
+        (url.pathname.startsWith('/assets/') || url.pathname === '/index.html'),
     new strategies.CacheFirst('cache-first')
 )
 
-// implements NetworkFirst to all routes	
-routing.registerRoute(
-    () => true,
-    new strategies.NetworkFirst('network-first'),
-)
+// implements NetworkFirst to all routes
+routing.registerRoute(() => true, new strategies.NetworkFirst('network-first'))
